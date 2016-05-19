5bdc1672a22d28f0b0d46a743d7c3157fd780d81
Evan Goode <mail@evangoo.de>
2016-05-19 08:32:05 -0400

    don't repeat yourself. bonus points for ultimate abstraction: write a program that simulates yourself programming your application.

--- client/all-posts/all-posts.js
+++ client/all-posts/all-posts.js
@@ -6,25 +6,7 @@ Template['all-posts'].helpers({
     return constants.Posts.find({}, {
       sort: {date: -1}
     });
-  },
-  left: function () {
-    return function () {
-      const position = JSON.parse(Session.get('position'));
-      return utilities.format(utilities.distance({
-          longitude: this.location.coordinates[0],
-          latitude: this.location.coordinates[1]
-        }, {
-          longitude: position.longitude,
-          latitude: position.latitude
-        }), constants.UNITS.distance);
-    }
-  },
-  center: function () {
-    return this.content;
-  },
-  right: function() {
-    return this.date;
-  },
+  }
 });
 Template['all-posts'].events({
   'click .template-item': function (event) {
