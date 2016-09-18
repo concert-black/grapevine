df67bd94bfd147bca5c2d55fd0aa490060b0ee07
Evan Goode <mail@evangoo.de>
2016-09-17 20:44:52 -0400

    cleanup mostly, some refactoring

--- both/constants.js
+++ both/constants.js
@@ -1,34 +1,33 @@
-export const POSTS = new Mongo.Collection('posts');
-export const MAX_DISTANCE = 8047; // meters
-export const POST_CHARACTER_LIMIT = 160; // characters
-export const POST_LIFE = 86400; // seconds
-export const LOCATION_TIMEOUT = 16384; // milliseconds
-export const RECONNECT_FREQUENCY = 1024; // milliseconds
-export const POSITION_VARIABILITY = 0.0005;
-export const UNITS = {
-  distance: [
-    {
-      short: 'ft',
-      long: 'feet',
-      value: 0.3048
-    }, {
-      short: 'mi',
-      long: 'miles',
-      value: 804.67
-    }
-  ]
+export const posts = new Mongo.Collection('posts');
+export const maxDistance = 8047; // meters
+export const postCharacterLimit = 160; // characters
+export const postLife = 86400; // seconds
+export const locationTimeout = 16384; // milliseconds
+export const reconnectFrequency = 1024; // milliseconds
+export const positionVariability = 0.0005;
+export const units = {
+	distance: [
+		{
+			short: 'ft',
+			long: 'feet',
+			value: 0.3048,
+		}, {
+			short: 'mi',
+			long: 'miles',
+			value: 804.67,
+		}
+	]
 };
-
-export const DEFAULT_SORT_METHOD = 'age';
-export const SORT_METHODS = [
-  {
-    value: 'age',
-    name:  'Age'
-  }, {
-    value: 'popularity',
-    name:  'Popularity'
-  }, {
-    value: 'distance',
-    name:  'Distance'
-  }
+export const defaultSortMethod = 'age';
+export const sortMethods = [
+	{
+		value: 'age',
+		name:	'Age',
+	}, {
+		value: 'popularity',
+		name:	'Popularity',
+	}, {
+		value: 'distance',
+		name:	'Distance',
+	}
 ];
